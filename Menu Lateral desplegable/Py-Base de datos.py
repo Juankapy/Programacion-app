3cce642821df782c2939e5b0e6524c5cc4b7480f
Juankapy <jpereavelasco@safareyes.es>
2024-11-28 14:17:22 +0100

    Conectando la base de datos
    se puede

---
+++ Menu Lateral desplegable/Py-Base de datos.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+config = {
+'user': 'root',
+'password': '1234 ',
+'host': 'localhost', # Puede ser 'localhost' si la base de datos está en tu máquina
+'database': 'prototipos',
+'port': '3307',
+'collation': 'utf8mb4_general_ci',
+}
+
+# Conectar a la base de datos
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()

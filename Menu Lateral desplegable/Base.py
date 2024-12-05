dc6827255fae2242c598f3db80c9f0b4b896f859
Juankapy <jpereavelasco@safareyes.es>
2024-12-05 10:05:45 +0100

    fuison

---
+++ Menu Lateral desplegable/Base.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+config = {
+    'user': 'root',
+    'password': '1234',
+    'host': 'localhost',
+    'database': 'prototipos',
+    'port': '3307',
+    'collation': 'utf8mb4_general_ci',
+}
+
+
+def fetch_all_data(query):
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]  # Obtén nombres de las columnas
+    conn.close()
+    return columns, data
+
+# Conectar a la base de datos
+
--- Menu Lateral desplegable/menu.py
+++ Menu Lateral desplegable/menu.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+<<<<<<< Updated upstream
 
 # Form implementation generated from reading ui file 'menu.ui'
 #
@@ -363,6 +364,94 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(_translate("MainWindow", "4"))
         self.pushButton.setText(_translate("MainWindow", "5"))
 
+=======
+from PyQt6 import QtCore, QtGui, QtWidgets, uic
+from PyQt6.QtCore import Qt, QPoint, QPropertyAnimation
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem
+from Base import fetch_all_data
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi("menu.ui", self)
+
+        self.bt_restaurar.setIcon(QtGui.QIcon("imagenes/new-tab.png"))
+        self.bt_minimizar.setIcon(QtGui.QIcon("imagenes/minimize.png"))
+        self.bt_maximizar.setIcon(QtGui.QIcon("imagenes/maximize.png"))
+        self.bt_cerrar.setIcon(QtGui.QIcon("imagenes/cancel.png"))
+        self.bt_tool.setIcon(QtGui.QIcon("imagenes/engranaje.png"))
+        self.bt_menu.setIcon(QtGui.QIcon("imagenes/hamburger.png"))
+        self.bt_BD.setIcon(QtGui.QIcon("imagenes/database.png"))
+
+        self.bt_restaurar.clicked.connect(self.showNormal)
+        self.bt_minimizar.clicked.connect(self.showMinimized)
+        self.bt_maximizar.clicked.connect(self.showMaximized)
+        self.bt_cerrar.clicked.connect(self.close)
+        self.bt_BD.clicked.connect(self.load_data)
+        self.bt_tool.clicked.connect(self.show_settings_page)
+        self.bt_menu.clicked.connect(self.toggle_side_panel)
+
+        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.resize(800, 600)
+        self.old_position = None
+
+        #self.stackedWidget.setCurrentIndex(0) Quiero que aparezca aqui la tabla principal
+
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.old_position = event.globalPosition().toPoint()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton and self.old_position:
+            delta = QPoint(event.globalPosition().toPoint() - self.old_position)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.old_position = event.globalPosition().toPoint()
+
+    def show_settings_page(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def toggle_side_panel(self):
+        current_width = self.frame_lateral.width()
+        target_width = 0 if current_width > 0 else 200
+
+        animation = QPropertyAnimation(self.frame_lateral, b"maximumWidth")
+        animation.setDuration(300)
+        animation.setStartValue(current_width)
+        animation.setEndValue(target_width)
+        animation.setEasingCurve(QtCore.QEasingCurve.Type.InOutQuad)
+        animation.start()
+
+        if self.is_panel_visible:
+            self.bt_menu.setIcon(QtGui.QIcon("imagenes/hamburger-open.png"))
+        else:
+            self.bt_menu.setIcon(QtGui.QIcon("imagenes/hamburger.png"))
+        self.is_panel_visible = not self.is_panel_visible
+
+    def load_data(self):
+
+        self.tableWidget.setGeometry(10, 10, 800, 400)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.is_panel_visible = True
+
+        query = ("SELECT * FROM empleados")
+        try:
+            columns, data = fetch_all_data(query)
+            self.display_data(columns, data)
+        except Exception as e:
+            print(f"Error al cargar datos: {e}")
+
+    def display_data(self, columns, data):
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(len(columns))
+        self.tableWidget.setHorizontalHeaderLabels(columns)
+
+        for row_num, row_data in enumerate(data):
+            self.tableWidget.insertRow(row_num)
+            for col_num, cell_data in enumerate(row_data):
+                self.tableWidget.setItem(row_num, col_num, QTableWidgetItem(str(cell_data)))
+        self.stackedWidget.setCurrentWidget(self.page_uno)
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     import sys
